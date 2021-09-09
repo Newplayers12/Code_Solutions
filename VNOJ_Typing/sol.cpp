1c2f6e2e43f8a62def9349415019ea9ebd5f9952
Newplayers12 <nhangiakham@gmail.com>
2021-09-09 12:31:42 +0700

    adding new problem solution

---
+++ VNOJ_Typing/sol.cpp
@@ -0,0 +1,15 @@
+/*
+    author:    Newplayers12
+    created:   Thursday 09-09-2021  12:31:20
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+int main()
+{
+    fastio;
+    
+    return 0;
+}
