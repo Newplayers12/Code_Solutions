9e16435089f452adbcf99fbdf0bc8d2b61553cb1
Newplayers12 <nhangiakham@gmail.com>
2021-09-09 15:16:43 +0700

    updated code

--- VNOJ_Typing/sol.cpp
+++ VNOJ_Typing/sol.cpp
@@ -7,9 +7,27 @@ using namespace std;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 
+
+const long long mod = 1e9 + 7;
+
 int main()
 {
     fastio;
-    
+    string s;
+    cin >> s;
+    int len = 0;
+    char last = s[0];
+    long long ans = 1;
+    for(int i = 0; i < (int) s.length(); ++i){
+        if (s[i] == last){
+            len++;
+        } else {
+            ans = (ans * len) % mod;
+            len = 1;
+            last = s[i];
+        }
+    }
+    ans = (ans * len) % mod;
+    cout << ans;
     return 0;
 }
