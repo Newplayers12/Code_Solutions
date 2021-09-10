e557d0489389ab99ed0c68ecb55e289cb78e09e9
iluvgirlswithglasses <58514512+iluvgirlswithglasses@users.noreply.github.com>
2021-09-10 19:17:33 +0700

    chỉnh sửa cho hợp với sol.pdf

--- VNOJ_hspc14h/sol.cpp
+++ VNOJ_hspc14h/sol.cpp
@@ -3,22 +3,21 @@
 #include <string>
 using namespace std;
 
-// see subproblem for more information 
 const int N = 1507;
-int m, n, mat[N][N], dpn[N][N], dpp[N][N];
+int m, n, f[N][N], gb[N][N], ga[N][N];
 
 void gendp() {
-	// basically the whole subproblem
+	// build ga[][] and gb[][]
 	for (int j = 1; j <= n; j++) {
 		for (int i = 1; i <= m; i++) {
-			dpn[i][j] = dpn[i-1][j];
-			if (mat[i][j] < 0) dpn[i][j] += -mat[i][j];
+			gb[i][j] = gb[i-1][j];
+			if (f[i][j] < 0) gb[i][j] += -f[i][j];
 		}
 	}
 	for (int j = 1; j <= n; j++) {
 		for (int i = m; i >= 1; i--) {
-			dpp[i][j] = dpp[i+1][j];
-			if (mat[i][j] > 0) dpp[i][j] += mat[i][j];
+			ga[i][j] = ga[i+1][j];
+			if (f[i][j] > 0) ga[i][j] += f[i][j];
 		}
 	}
 }
@@ -26,14 +25,6 @@ void gendp() {
 /**
  * drivers
  * */
-void printmat() {
-	for (int i = 1; i <= m; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << mat[i][j] << " ";
-		} cout << endl;
-	}
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0);
 	cin >> m >> n;
@@ -43,30 +34,30 @@ int main() {
 			int v = stoi(cell.substr(1, cell.length() - 1));
 			//
 			if (cell.at(0) == 'A') {
-				mat[i][j] = v;
+				f[i][j] = v;
 			} else {
-				mat[i][j] = -v;
+				f[i][j] = -v;
 			}
 		}
 	}
 	//
 	gendp();
 	for (int i = 1; i <= m; i++) {
-		mat[i][1] = dpp[i+1][1];
+		// i = 1 is kinda the edge case
+		f[i][1] = ga[i+1][1];
 	}
 	//
 	for (int i = 1; i <= m; i++) {
 		//
 		for (int j = 2; j <= n; j++) {
 			int pre[3] = {
-				mat[i][j-1] + dpn[i-1][j] + dpp[i+1][j],	// left -> right
-				mat[i-1][j] - dpp[i][j] + dpp[i+1][j],		// top -> down	!FIX
-				mat[i-1][j-1] + dpn[i-1][j] + dpp[i+1][j]	// tl -> dr
+				f[i][j-1] + gb[i-1][j] + ga[i+1][j],	// left -> right
+				f[i-1][j] - ga[i][j] + ga[i+1][j],		// top -> down	!FIX
+				f[i-1][j-1] + gb[i-1][j] + ga[i+1][j]	// tl -> dr
 			};
-			mat[i][j] = *max_element(pre, pre+3);
+			f[i][j] = *max_element(pre, pre+3);
 		}
 	}
-	cout << mat[m][n] << endl;
-	// printmat();
+	cout << f[m][n] << endl;
 	return 0;
 }
