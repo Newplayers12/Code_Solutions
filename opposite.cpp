fc28ac705ef49ef4a6ea427616238c20bb4892bd
iluvgirlswithglasses <58514512+iluvgirlswithglasses@users.noreply.github.com>
2021-09-12 11:13:14 +0700

    Add files via upload

---
+++ opposite.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int a, b, c;
+
+int calc() {
+	a--; b--; c--;
+	//
+	int h = abs(a - b), 
+		f = h << 1, 
+		d = (c + h) % f;
+	if (a >= f || b >= f || c >= f) return -1;
+	//
+	return (c + h) % f + 1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		cin >> a >> b >> c;
+		cout << calc() << "\n";
+	}
+	return 0;
+}
