3db8744a8e884a87d5b0620b58d5db95dd8e1533
Newplayers12 <nhangiakham@gmail.com>
2021-10-05 23:15:56 +0700

    update new code

--- Codeforces Problems/Codeforces Round 746/A.cpp
+++ Codeforces Problems/Codeforces Round 746/A.cpp
@@ -21,6 +21,10 @@
                                        .............................................
                                               Buddha bless, never bug
 */
+/*
+    author:    Newplayers12
+    created:   Tuesday 05-10-2021  20:59:21
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -29,28 +33,23 @@ using namespace std;
 int main()
 {
     fastio;
-    string s;
-    cin >> s;
     int test;
     cin >> test;
-    int id = 0;
     while(test--){
-        id++;
-        string t;
-        cin >> t;
-        if (t.length() == s.length()){
-            int dem = 0;
-            for(int i = 0; i < s.length(); ++i){
-                if (s[i] != t[i]) dem++;
-                if (dem > 2){
-                    break;
-                }
-            }
-            if (dem>2){
-                continue;
-            }
-            cout << id << ' ';
-        }
+        int n, h;
+        cin >> n >> h;
+        vector<int> a(n);
+        for(int &x:a) cin >> x;
+        sort(a.rbegin(), a.rend());
+        int ans = h/(a[0] + a[1]);
+        ans <<= 1;
+        h %= (a[0] + a[1]);
+        if (h == 0){
+            cout << ans;
+        } else if (h <= a[0]){
+            cout << ans + 1;
+        } else cout << ans + 2;
+        cout << '\n';
     }
     return 0;
 }
